b38d3c55351fd6cb054d3b49cab68842aa98fc28
xj-m <xjm@Jhu.edu>
2020-04-17 02:50:21 -0400

    support filter on apartment

--- UrHome-app/src/components/NeighborhoodCardList.js
+++ UrHome-app/src/components/NeighborhoodCardList.js
@@ -31,6 +31,12 @@ const NeighborhoodCardList = () => {
   const [visible, setVisible] = useState(true);
   const [selectedNeighborhood, setSelectedNeighborhood] = useState("Walbrook");
 
+  const [inputBedNum, setInputBedNum] = useState(2);
+  const [inputBathNum, setInputBathNum] = useState(2);
+  const [inputRentFrom, setInputRentFrom] = useState(500);
+  const [inputRentTo, setInputRentTo] = useState(2000);
+
+
   const neighborhoods = useSelector((state) => state.apartment.neighborhoods);
   const displayApartments = useSelector((state) => {
     return state.apartment.apartments.filter(
@@ -51,6 +57,54 @@ const NeighborhoodCardList = () => {
   // useEffect(() => {
   //   console.log(apartments);
   // },[apartments])
+  // const [inputHomeType, setInputHomeType] = useState()
+
+  const SearchSection = () => {
+    function onChange() {}
+    return (
+      <div className="search-section">
+        <div className="single-search-input">
+          Bedrooms
+          <Input
+            size="small"
+            style={{ width: "100px", marginLeft: "10px" }}
+            value={inputBedNum}
+            onChange={setInputBedNum}
+          />
+        </div>
+        <div className="single-search-input">
+          Bathrooms
+          <Input
+            size="small"
+            style={{ width: "100px", marginLeft: "10px" }}
+            value={inputBathNum}
+            onChange={setInputBathNum}
+          />
+        </div>
+        <div className="single-search-input">
+          Rent
+          <Input
+            size="small"
+            style={{ width: "50px", margin: "0 10px" }}
+            value={inputRentFrom}
+            onChange={setInputRentFrom}
+          />
+          to
+          <Input
+            size="small"
+            style={{ width: "50px", margin: "0 10px" }}
+            value={inputRentTo}
+            onChange={setInputRentTo}
+          />
+        </div>
+        {/* <div className="single-search-input">
+          Home Types
+          <Input size="small" style={{ width: "100px", marginLeft: "10px" }} />
+        </div> */}
+        <Button>Search</Button>
+      </div>
+    );
+  };
 
   const neighborhoodRenderItem = (item) => {
     const neighborhood = item;
@@ -97,20 +151,20 @@ const NeighborhoodCardList = () => {
       // <Popover content={content} title="Title" trigger="click"></Popover>
       <div onClick={apartmentCardOnClick}>
         {/* <Card hoverable className="house-single-card"> */}
-          <Card>
+        <Card>
           {/* <div className="house-card-img-container"> */}
-            <img className="house-card-img" alt="example" src={imageSrc} />
+          <img className="house-card-img" alt="example" src={imageSrc} />
           {/* </div> */}
           {/* <div className="house-card-text-container"> */}
-            <div className="card-title">{apartment.type}</div>
-            <br />
-            <Text>number of beds: {apartment.bedsNum}</Text>
-            <br />
-            <Text>Price:{apartment.price}</Text>
-            <br />
-            <Text>address:{apartment.address}</Text>
-            <br />
-            <Text>location:{apartment.location.lat}</Text>
+          <div className="card-title">{apartment.type}</div>
+          <br />
+          <Text>number of beds: {apartment.bedsNum}</Text>
+          <br />
+          <Text>Price:{apartment.price}</Text>
+          <br />
+          <Text>address:{apartment.address}</Text>
+          <br />
+          <Text>location:{apartment.location.lat}</Text>
           {/* </div> */}
         </Card>
       </div>
@@ -135,30 +189,6 @@ const NeighborhoodCardList = () => {
           renderItem={apartmentRenderItem}
         />
       )}
-    </div>
-  );
-
-  const SearchSection = () => (
-    <div className="search-section">
-      <div className="single-search-input">
-        Bedrooms
-        <Input size="small" style={{ width: "100px", marginLeft: "10px" }} />
-      </div>
-      <div className="single-search-input">
-        Bathrooms
-        <Input size="small" style={{ width: "100px", marginLeft: "10px" }} />
-      </div>
-      <div className="single-search-input">
-        Rent
-        <Input size="small" style={{ width: "50px", margin: "0 10px" }} />
-        to
-        <Input size="small" style={{ width: "50px", margin: "0 10px" }} />
-      </div>
-      <div className="single-search-input">
-        Home Types
-        <Input size="small" style={{ width: "100px", marginLeft: "10px" }} />
-      </div>
-      <Button>Search</Button>
     </div>
   );
 
